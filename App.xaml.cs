55313038e18b0f5e4ba8ec3d45b600b5de87e4e4
Pustan Radu <mp_radu@yahoo.com>
2024-12-07 12:53:36 +0200

    Laborator9

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace PustanRaduLab7
+﻿using PustanRaduLab7.Data;
+
+namespace PustanRaduLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
